c142717d8f0ae4f3deca7de1664ec31cce2fa24c
Alessandra Lopes <alessandra.lopesiqp@gmail.com>
2022-10-17 21:53:11 -0300

    Criando classe com string de conexão ao banco de dados

---
+++ Model/AppModelo.Model.Infra/DataBase/Database.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppModelo.Model.Infra.DataBase
+{
+    public static class Database
+    {
+        public static string MySqlConection()
+        {
+            var conn = "server=mysql.wwonline.com.br;database=wwonline02;uid=wwonline02;password=aluno22senai;";
+            return conn;
+        }
+    }
+}
