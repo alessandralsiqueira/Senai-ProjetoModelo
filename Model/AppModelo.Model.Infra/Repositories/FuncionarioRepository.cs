b3505493babeb2dbdf507d3428952b2abc0e05a2
alessandralsiqueira <alessandra.lopesiqp@gmail.com>
2022-11-17 21:56:30 -0300

    Criado frmListaFuncionario

--- Model/AppModelo.Model.Infra/Repositories/FuncionarioRepository.cs
+++ Model/AppModelo.Model.Infra/Repositories/FuncionarioRepository.cs
@@ -9,6 +9,25 @@ namespace AppModelo.Model.Infra.Repositories
 {
     public class FuncionarioRepository
     {
+        /// <summary>
+        /// esse método faz uma inserção no banco de dados com os parâmetros 
+        /// </summary>
+        /// <param name="nome"></param>
+        /// <param name="dataNascimento"></param>
+        /// <param name="sexo"></param>
+        /// <param name="email"></param>
+        /// <param name="telefone"></param>
+        /// <param name="telefone_contato"></param>
+        /// <param name="cep"></param>
+        /// <param name="logradouro"></param>
+        /// <param name="numero"></param>
+        /// <param name="complemento"></param>
+        /// <param name="bairro"></param>
+        /// <param name="municipio"></param>
+        /// <param name="uf"></param>
+        /// <param name="nacionalidade"></param>
+        /// <param name="naturalidade"></param>
+        /// <returns></returns>
         public bool Inserir(string nome, DateTime dataNascimento, bool sexo, string email, string telefone, string telefone_contato, string cep, string logradouro, int numero, string complemento, string bairro, string municipio, string uf, int nacionalidade, int naturalidade)
         {
             var dataConvertida = dataNascimento.ToString("yyyy-MM-dd");
